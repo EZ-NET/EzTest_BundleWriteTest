c93c16161c301e9693a20bc7bdd7e904b15dcfd4
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-03-31 10:29:16 +0900

    Read and display Info.plist again. And refactoring.

--- BundleWriteTest/AppDelegate.m
+++ BundleWriteTest/AppDelegate.m
@@ -8,57 +8,31 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
+
+    // Get path of Info.plist file in the Bundle.
     NSString* plist = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
-    NSDictionary* info = [NSDictionary dictionaryWithContentsOfFile:plist];
     
+    // Read from Info.plist file.
+    NSDictionary* info = [NSDictionary dictionaryWithContentsOfFile:plist];
     NSLog(@"%@", info);
     
+    // Write to Info.plist file.
     NSMutableDictionary* rewrite = info.mutableCopy;
     
     rewrite[@"CFBundleDisplayName"] = @"Bundle Rewrite";
     
-    if ([rewrite writeToFile:plist atomically:YES])
-    {
-        NSLog(@"OK");
-    }
-    else
-    {
-        NSLog(@"NG");
-    }
+    BOOL succeeded = [rewrite writeToFile:plist atomically:YES];
+    NSLog(succeeded ? @"OK" : @"NG");
+    
+    // Re-read from Info.plist file.
+    NSDictionary* info2 = [NSDictionary dictionaryWithContentsOfFile:plist];
+    NSLog(@"%@", info2);
     
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
